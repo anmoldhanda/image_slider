ce20d539791b5c3d8ae7515b6e2dd59ab8ada6de
anmol dhanda <rk352714@gmail.com>
2024-04-05 22:15:46 +0530

    slider indicating text

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ let current_slide_index = 0;
 
 const previous_slide = () => {
   if (current_slide_index === 0) {
-    // if the current slide index is at 0th position when the previous icon is clicked then return the last slide
+    // =============================== if the current slide index is at 0th position when the previous icon is clicked then return the last slide ===============================
     current_slide_index = slides.length - 1;
   } else {
-    // else if the current slide index isn't at 0th position then decrement the current slide index's slides with (-= 1) to get the previous slide and update the image slider container containing slide's position with the help of current slide index
+    // =============================== else if the current slide index isn't at 0th position then decrement the current slide index's slides with (-= 1) to get the previous slide and update the image slider container containing slide's position with the help of current slide index ===============================
     current_slide_index -= 1;
   }
   console.log("previous");
@@ -19,10 +19,10 @@ const previous_slide = () => {
 
 const next_slide = () => {
   if (current_slide_index === slides.length - 1) {
-    // if the current slide index is at the last index position when the next icon is clicked then return the first slide
+    // =============================== if the current slide index is at the last index position when the next icon is clicked then return the first slide ===============================
     current_slide_index = 0;
   } else {
-    // else if the current slide index isn't at the last index position then increment the current slide index with (+= 1) to get the next slide and update the image slider container containing slide's position with the help of current slide index
+    // =============================== else if the current slide index isn't at the last index position then increment the current slide index with (+= 1) to get the next slide and update the image slider container containing slide's position with the help of current slide index ===============================
     current_slide_index += 1;
   }
   images_slider.style.transform = `translateX(-${current_slide_index * 100}%)`;
